5be04aaddee4bc94b319a8788e1eeae7c575f056
SiddharthV147 <siddharthvarade147@gmail.com>
2025-12-29 22:13:01 +0530

    Server Initialized

---
+++ semantic_cache/adapter/api.py
@@ -0,0 +1,8 @@
+def put(prompt: str, answer: str) -> None:
+    print("prompt :", prompt, " answer :",answer)
+
+def get() -> str:
+    return "GET adapter"
+
+def flush() -> None:
+    pass
---
+++ server/server.py
@@ -0,0 +1,44 @@
+import os
+import json
+import argparse
+
+import uvicorn
+from pydantic import BaseModel
+from fastapi import FastAPI, Request, HTTPException
+
+from semantic_cache.adapter.api import put, get, flush
+
+app = FastAPI()
+
+
+class CacheData(BaseModel):
+    prompt: str
+    answer: str
+
+
+@app.get("/")
+async def default():
+    return "Semntic cache running"
+
+
+@app.post("/put")
+async def cache_put(cache_data: CacheData):
+    put(cache_data.prompt, cache_data.answer)
+    return "successfully updated the cache"
+
+
+def main():
+    from starlette.middleware.cors import CORSMiddleware
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    uvicorn.run(app, port=8000)
+
+
+if __name__ == "__main__":
+    main()
